9100545954062bcbc90c9223e1d2bff8158f4ecc
Pawan Kumar <surpawan@gmail.com>
2021-09-07 12:24:20 +0530

    Added Logic

--- src/App.js
+++ src/App.js
@@ -1,92 +1,66 @@
-import { useState } from "react";
-import { currencies } from "./data/currency";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import CurrencyConvertor from "./components/CurrencyConvertor";
+
+const API_URL = "https://open.er-api.com/v6/latest";
 
 const App = () => {
-  const [convertedValue, setConvertedValue] = useState("");
+  const [exchangeData, setExchangeData] = useState([]);
   const [amount, setAmount] = useState("");
+  const [convertAmount, setConvertedAmount] = useState("");
   const [fromCurrency, setFromCurrency] = useState("USD");
   const [toCurrency, setToCurrency] = useState("INR");
+
+  useEffect(() => {
+    const fetchExchangeData = async () => {
+      const { data } = await axios.get(`${API_URL}/${fromCurrency}`);
+      setExchangeData(data.rates);
+    };
+
+    fetchExchangeData();
+  }, [fromCurrency]);
+
+  useEffect(() => {
+    if (amount) {
+      // const fromCurrencyRate = exchangeData[fromCurrency];
+      const toCurrencyRate = exchangeData[toCurrency];
+
+      setConvertedAmount(amount * toCurrencyRate);
+    }
+  }, [amount, toCurrency, exchangeData]);
 
   return (
     <div className="bg-gradient-to-r from-purple-400 via-pink-500 to-red-500">
       <div className="container mx-auto">
         <div className="flex items-center justify-center h-screen">
-          <div className="flex flex-col bg-white shadow-md rounded w-max h-max h-96 h-max px-6 pt-10 pb-8">
+          <div className="flex flex-col bg-black bg-opacity-20 shadow-md rounded w-max h-max h-96 h-max px-6 pt-10 pb-8">
             <div className="text-center w-full mb-10">
-              <p className="text-6xl font-thin">Currency Convertor</p>
+              <p className="text-6xl font-thin text-white">
+                Currency Convertor
+              </p>
             </div>
+            <CurrencyConvertor
+              amount={amount}
+              fromCurrency={fromCurrency}
+              toCurrency={toCurrency}
+              setAmount={(value) => setAmount(value)}
+              setFromCurrency={(value) => setFromCurrency(value)}
+              setToCurrency={(value) => setToCurrency(value)}
+            />
 
-            {/* Input Blocks */}
-            <div className="flex space-x-2 mb-10">
-              <div className="flex flex-col">
-                <label
-                  for="amount"
-                  class="block text-sm font-medium text-gray-700"
-                >
-                  Amount
-                </label>
-                <input
-                  type="text"
-                  name="amount"
-                  placeholder="0.00"
-                  id="amount"
-                  className="mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                  value={amount}
-                  onChange={(e) => setAmount(e.target.value)}
-                />
-              </div>
-              <div className="flex flex-col">
-                <label
-                  htmlFor="currency-from"
-                  class="block text-sm font-medium text-gray-700"
-                >
-                  From
-                </label>
-                <select
-                  name="currency-from"
-                  id="currency-from"
-                  className="mt-1 focus:ring-blue-500 focus:border-blue-500 shadow-sm sm:text-sm border-gray-300 rounded-md"
-                  value={fromCurrency}
-                >
-                  {currencies &&
-                    currencies.map((currency) => (
-                      <option
-                        value={currency.value}
-                      >{`${currency.value} - ${currency.label}`}</option>
-                    ))}
-                </select>
-              </div>
-              <div className="flex flex-col">
-                <label
-                  htmlFor="currency-to"
-                  class="block text-sm font-medium text-gray-700"
-                >
-                  To
-                </label>
-                <select
-                  name="currency-to"
-                  id="currency-to"
-                  className="mt-1 focus:ring-blue-500 focus:border-blue-500 shadow-sm sm:text-sm border-gray-300 rounded-md"
-                  value={toCurrency}
-                >
-                  {currencies &&
-                    currencies.map((currency) => (
-                      <option
-                        value={currency.value}
-                      >{`${currency.value} - ${currency.label}`}</option>
-                    ))}
-                </select>
-              </div>
-            </div>
             {/* Result Block */}
-            <div className="flex items-end justify-between">
+            <div className="flex items-end justify-between text-white">
               {amount ? (
                 <div>
-                  <div>100 Canadian Dollar =</div>
-                  <div className="text-5xl font-bold">80.84 USD</div>
+                  <div>
+                    {parseInt(amount).toFixed(2)} {fromCurrency} =
+                  </div>
+                  <div className="text-5xl font-bold">
+                    {parseInt(convertAmount).toFixed(2)} {toCurrency}
+                  </div>
                 </div>
               ) : (
-                <div className="text-5xl">No Amount Given.</div>
+                ""
               )}
             </div>
           </div>
---
+++ src/components/CurrencyConvertor.js
@@ -0,0 +1,82 @@
+import { currencies } from "../data/currency";
+
+const CurrencyConvertor = ({
+  amount,
+  fromCurrency,
+  toCurrency,
+  setAmount,
+  setFromCurrency,
+  setToCurrency,
+}) => {
+  return (
+    <>
+      <div className="flex space-x-2 mb-10">
+        <div className="flex flex-col">
+          <label
+            htmlFor="amount"
+            className="block text-sm font-medium text-white"
+          >
+            Amount
+          </label>
+          <input
+            type="text"
+            name="amount"
+            placeholder="0.00"
+            id="amount"
+            className="mt-1 focus:ring-yellow-500 focus:border-yellow-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+          />
+        </div>
+        <div className="flex flex-col">
+          <label
+            htmlFor="currency-from"
+            className="block text-sm font-medium text-white"
+          >
+            From
+          </label>
+          <select
+            name="currency-from"
+            id="currency-from"
+            className="mt-1 focus:ring-yellow-500 focus:border-yellow-500 shadow-sm sm:text-sm border-gray-300 rounded-md"
+            value={fromCurrency}
+            onChange={(e) => setFromCurrency(e.target.value)}
+          >
+            {currencies &&
+              currencies.map((currency, index) => (
+                <option
+                  key={index}
+                  value={currency.value}
+                >{`${currency.value} - ${currency.label}`}</option>
+              ))}
+          </select>
+        </div>
+        <div className="flex flex-col">
+          <label
+            htmlFor="currency-to"
+            className="block text-sm font-medium text-white"
+          >
+            To
+          </label>
+          <select
+            name="currency-to"
+            id="currency-to"
+            className="mt-1 focus:ring-yellow-500 focus:border-yellow-500 shadow-sm sm:text-sm border-gray-300 rounded-md"
+            value={toCurrency}
+            onChange={(e) => setToCurrency(e.target.value)}
+          >
+            {currencies &&
+              currencies.map((currency, index) => (
+                <option
+                  key={index}
+                  value={currency.value}
+                >{`${currency.value} - ${currency.label}`}</option>
+              ))}
+          </select>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CurrencyConvertor;
